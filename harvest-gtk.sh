51e15ae8ca033c4b9a504b3a227f43edf6ccc268
Tomasz Kowalewski <me@tkowalewski.pl>
2012-12-02 00:23:18 +0100

    Small improvement

--- harvest-gtk.sh
+++ harvest-gtk.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-valac-0.18 --pkg gee-1.0 --pkg granite --pkg gtk+-3.0 --pkg libxml-2.0 --pkg libsoup-2.4 --pkg json-glib-1.0 ./harvest-gtk.vala
+valac-0.18 --pkg gee-1.0 --pkg gtk+-3.0 --pkg libxml-2.0 --pkg libsoup-2.4 ./harvest-gtk.vala
